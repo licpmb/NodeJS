b183886da4b4fd0fbf2c402b49e3a3cc19eba074
Pamela Murias Bauza <licpmb@gmail.com>
2025-07-18 01:10:46 -0300

    model

--- src/services/products.services.js
+++ src/services/products.services.js
@@ -1,21 +1,21 @@
 import * as model from "../models/products.model.js";
 
 export const getAllProducts = async () => {
-  return await Model.getAllProducts();
+  return await model.getAllProducts();
 };
 
 export const getProductById = async (id) => {
-  return await Model.getProductById(id);
+  return await model.getProductById(id);
 };
 
 export const createProduct = async (newProduct) => {
-  return await Model.createProduct(newProduct);
+  return await model.createProduct(newProduct);
 };
 
 export const updateProduct = async (id, updatedProductData) => {
-  return await Model.updateProduct(id, updatedProductData);
+  return await model.updateProduct(id, updatedProductData);
 };
 
 export const deleteProduct = async (id) => {
-  return await Model.deleteProduct(id);
+  return await model.deleteProduct(id);
 };
